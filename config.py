1a0d031d4fa38c9d19bc8e2ab60262ca1fdde401
KAJ4 <40004347+KAJdev@users.noreply.github.com>
2019-06-28 16:15:58 -0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = "bot token"
-URI = "mongo connection token"
-DEFAULT_PREFIX = [">", "_"]
-TICK_SPEED = 10
-DEBUG_STATUS = False
-shop_items = [{'name': "GTX 1060", 'type': 'gpu', 'system': 5, 'cost': 50000}, {'name': "AMD Athlon II X3", 'type': 'cpu', 'system': 2, 'cost': 15000}, {'name': "Intel core i3", 'type': "cpu", 'system': 4, 'cost': 15000}, {'name': "4GB RAM Stick", 'type': 'ram', 'system': 4, 'cost': 40000}, {'name': "8GB RAM Stick", 'type': 'ram', 'system': 8, 'cost': 90000}, {'name': "16GB RAM Stick", 'type': 'ram', 'system': 16, 'cost': 200000}, {'name': "Intel core i5", 'type': "cpu", 'system': 5, 'cost': 35000}, {'name': "Intel core i7", 'type': "cpu", 'system': 6, 'cost': 50000}, {'name': "Intel Xeon", 'type': "cpu", 'system': 9, 'cost': 200000}, {'name': "AMD Threadripper", 'type': "cpu", 'system': 9, 'cost': 190000}, {'name': "AMD Radeon RX 580", 'type': 'gpu', 'system': 6, 'cost': 140000}, {'name': "Nvidia GeForce GTX 1070", 'type': 'gpu', 'system': 7, 'cost': 170000}, {'name': "Nvidia GeForce RTX 2080 Ti", 'type': 'gpu', 'system': 10, 'cost': 250000}]
