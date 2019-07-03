58f4452129113f3ce2e233a4f7466104d9f961ac
KAJ4 <40004347+KAJdev@users.noreply.github.com>
2019-07-03 15:20:27 -0800

    message updates

--- cogs/main_commands.py
+++ cogs/main_commands.py
@@ -887,7 +887,7 @@ class main_commands(commands.Cog):
                         self.users_col.update_one({'user_id': str(ctx.author.id)}, {'$set':{'balance': user['balance'] - item['cost'], 'inventory': user['inventory']}})
 
                         #send confirmation message
-                        await ctx.author.send("`LOG: (store.gov)` You have just purchased " + id + " for " + str(item['cost']) + "` <:coin:592831769024397332> `. You can view it in your Inventory.`")
+                        await ctx.author.send("`LOG: (store.gov) You have just purchased " + id + " for " + str(item['cost']) + " ` <:coin:592831769024397332>`. You can view it in your Inventory.`")
 
                         return
                     else:
